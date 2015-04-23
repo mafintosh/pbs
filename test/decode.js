bc6dcc9e501aa8728d4563394342b982752cf31e
Mathias Buus <mathiasbuus@gmail.com>
2015-04-22 17:39:34 -0700

    split tests into encode/decode and add optional tests

---
+++ test/decode.js
@@ -0,0 +1,98 @@
+var pbs = require('../')
+var protobuf = require('protocol-buffers')
+var tape = require('tape')
+var concat = require('concat-stream')
+var fs = require('fs')
+
+var schema = fs.readFileSync(__dirname + '/test.proto')
+var messages = pbs(schema)
+var pbMessages = protobuf(schema)
+
+tape('decodes', function (t) {
+  t.plan(1)
+
+  var decoder = messages.Test.decode()
+
+  decoder.hello(function (hello, cb) {
+    t.same(hello, 'world', 'got hello world')
+  })
+
+  decoder.on('finish', function () {
+    t.end()
+  })
+
+  var buf = pbMessages.Test.encode({
+    hello: 'world'
+  })
+
+  decoder.end(buf)
+})
+
+tape('decodes required field only once', function (t) {
+  t.plan(1)
+
+  var decoder = messages.Test.decode()
+
+  decoder.hello(function (hello, cb) {
+    t.same(hello, 'world', 'got hello world')
+    cb()
+  })
+
+  decoder.on('finish', function () {
+    t.end()
+  })
+
+  var buf = pbMessages.TestRepeated.encode({
+    hello: ['world', 'world2']
+  })
+
+  decoder.end(buf)
+})
+
+tape('decodes repeated', function (t) {
+  t.plan(3)
+
+  var decoder = messages.TestRepeated.decode()
+  var expected = ['a', 'b', 'c']
+
+  decoder.hello(function (hello, cb) {
+    t.same(hello, expected.shift())
+    cb()
+  })
+
+  decoder.on('finish', function () {
+    t.end()
+  })
+
+  var buf = pbMessages.TestRepeated.encode({
+    hello: expected
+  })
+
+  decoder.end(buf)
+})
+
+tape('decoder fails if missing required message', function (t) {
+  t.plan(1)
+
+  var decoder = messages.Test.decode()
+
+  decoder.on('error', function (err) {
+    t.ok(err, 'had error')
+    t.end()
+  })
+
+  decoder.end()
+})
+
+tape('decoder allows missing optional message', function (t) {
+  t.plan(1)
+
+  var decoder = messages.TestOptional.decode()
+
+  decoder.on('finish', function (err) {
+    t.ok(true, 'everthing is fine')
+    t.end()
+  })
+
+  decoder.end()
+})
--- test/encode.js
+++ test/encode.js
@@ -64,78 +64,15 @@ tape('encoder fails if missing required message', function (t) {
   encoder.finalize()
 })
 
-tape('decodes', function (t) {
+tape('encoder allows missing optional message', function (t) {
   t.plan(1)
 
-  var decoder = messages.Test.decode()
+  var encoder = messages.TestOptional.encode()
 
-  decoder.hello(function (hello, cb) {
-    t.same(hello, 'world', 'got hello world')
-  })
-
-  decoder.on('finish', function () {
+  encoder.on('end', function (err) {
+    t.ok(true, 'everthing is fine')
     t.end()
   })
 
-  var buf = pbMessages.Test.encode({
-    hello: 'world'
-  })
-
-  decoder.end(buf)
-})
-
-tape('decodes required field only once', function (t) {
-  t.plan(1)
-
-  var decoder = messages.Test.decode()
-
-  decoder.hello(function (hello, cb) {
-    t.same(hello, 'world', 'got hello world')
-    cb()
-  })
-
-  decoder.on('finish', function () {
-    t.end()
-  })
-
-  var buf = pbMessages.TestRepeated.encode({
-    hello: ['world', 'world2']
-  })
-
-  decoder.end(buf)
-})
-
-tape('decodes repeated', function (t) {
-  t.plan(3)
-
-  var decoder = messages.TestRepeated.decode()
-  var expected = ['a', 'b', 'c']
-
-  decoder.hello(function (hello, cb) {
-    t.same(hello, expected.shift())
-    cb()
-  })
-
-  decoder.on('finish', function () {
-    t.end()
-  })
-
-  var buf = pbMessages.TestRepeated.encode({
-    hello: expected
-  })
-
-  decoder.end(buf)
-})
-
-tape('decoder fails if missing required message', function (t) {
-  t.plan(1)
-
-  var decoder = messages.Test.decode()
-
-  decoder.on('error', function (err) {
-    t.ok(err, 'had error')
-    t.end()
-  })
-
-  decoder.end()
+  encoder.finalize()
 })
